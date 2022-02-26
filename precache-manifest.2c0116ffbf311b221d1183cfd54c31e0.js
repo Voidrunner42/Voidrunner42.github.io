5a24a6374d67123c14041b608614127830ed1bf9
Voidrunner42 <61822888+Voidrunner42@users.noreply.github.com>
2022-02-26 15:29:50 +0100

    Add files via upload

---
+++ precache-manifest.2c0116ffbf311b221d1183cfd54c31e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cdc3d6a3d573fad2b44e018431b593",
+    "url": "/BetterDiscordStuff/index.html"
+  },
+  {
+    "revision": "15a419f2c75d872e4d1d",
+    "url": "/BetterDiscordStuff/static/css/main.1359096b.chunk.css"
+  },
+  {
+    "revision": "f8768f816958eb3413de",
+    "url": "/BetterDiscordStuff/static/js/2.b6fd7b9e.chunk.js"
+  },
+  {
+    "revision": "8ec8b65940d4527c3e1ef85a62fa0e11",
+    "url": "/BetterDiscordStuff/static/js/2.b6fd7b9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15a419f2c75d872e4d1d",
+    "url": "/BetterDiscordStuff/static/js/main.c3275b09.chunk.js"
+  },
+  {
+    "revision": "761c453297bd818950be",
+    "url": "/BetterDiscordStuff/static/js/runtime-main.67e26751.js"
+  }
+]);
